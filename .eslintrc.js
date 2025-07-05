44564a1b1e03baca00cd0c28919523013a9a3b61
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-07-05 23:16:28 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   rules: {
-    // ✅ Allow temporary use of 'any' for faster development
     '@typescript-eslint/no-explicit-any': 'off',
-
-    // ✅ Allow unused vars (for in-progress features)
     '@typescript-eslint/no-unused-vars': 'off',
-
-    // ⚠️ Just warn about image tag
     '@next/next/no-img-element': 'warn',
-
-    // ⚠️ Just warn about missing dependencies in useEffect
     'react-hooks/exhaustive-deps': 'warn',
   }
 };
