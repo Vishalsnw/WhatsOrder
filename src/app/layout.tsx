ed0a5e90e424aa3f175fff04a394704babb41711
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-07-05 23:38:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+// src/app/layout.tsx
+
 import './globals.css';
+import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
+import { AuthProvider } from '@/context/AuthContext';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'WhatsOrder',
@@ -10,7 +16,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
